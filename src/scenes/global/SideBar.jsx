58943164b7c71cacb853a15f2e010ea0ed9136d0
trCVZ <tristan.cvz.pro@gmail.com>
2024-04-20 18:07:35 +0200

    Added profile user

--- src/scenes/global/SideBar.jsx
+++ src/scenes/global/SideBar.jsx
@@ -23,8 +23,66 @@ import MapOutlined from "@mui/icons-material/MapOutlined";
 function SideBar() {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [selected, setSelected] = useState("Dashboard");
 
-  return <div>SideBar</div>;
+  return (
+    <Box
+      sx={{
+        "& .pro-sidebar-inner": {
+          background: `&{colors.primary[400]}`,
+        },
+        "& .pro-icon-wrapper": {
+          backgroundColor: "transparent",
+        },
+        "& .pro-inner-item": {
+          padding: "5px 35px 5px 20px",
+        },
+        "& .pro-menu-item-hover": {
+          color: "#868dfb",
+        },
+        "& .pro-menu-item.active": {
+          color: "#6870fa",
+        },
+      }}
+    >
+      <Menu>
+        <MenuItem></MenuItem>
+
+        {!isCollapsed && (
+          <Box
+            mb="25px"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Box>
+              <img
+                alt="profile-user"
+                width="100px"
+                height="100px"
+                src={`../../assets/user.jpg`}
+                style={{ cursor: "pointer", borderRadius: "50%" }}
+              />
+            </Box>
+            <Box textAlign="center">
+              <Typography
+                variant="h2"
+                color={colors.grey[100]}
+                fontWeight="bold"
+                sx={{ m: "10px 0 0 0" }}
+              >
+                trCVZ
+              </Typography>
+              <Typography variant="h5" color={colors.greenAccent[500]}>
+                Admin
+              </Typography>
+            </Box>
+          </Box>
+        )}
+      </Menu>
+    </Box>
+  );
 }
 
 export default SideBar;
