e42f24ed4148a2e88679416fa42194f60a27ea5c
trCVZ <tristan.cvz.pro@gmail.com>
2024-05-18 19:02:26 +0200

    Styled datagrid

--- src/scenes/global/SideBar.jsx
+++ src/scenes/global/SideBar.jsx
@@ -4,7 +4,7 @@ import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
 import "react-pro-sidebar/dist/css/styles.css";
 
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
-import { Link } from "@mui/material";
+import { Link } from "react-router-dom";
 import { tokens } from "../../theme";
 
 import HomeOutlined from "@mui/icons-material/HomeOutlined";
--- src/scenes/team/index.jsx
+++ src/scenes/team/index.jsx
@@ -11,46 +11,52 @@ const Team = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const columns = [
-    { field: "id", headerName: "ID" },
+    { field: "id", headerName: "ID", headerClassName: "headerColumns" },
     {
       field: "name",
       headerName: "Name",
+      headerClassName: "headerColumns",
       flex: 1,
-      cellClassName: "name-column-cell",
+      cellClassName: "name-column--cell",
     },
     {
       field: "age",
       headerName: "Age",
+      headerClassName: "headerColumns",
       type: "number",
       headerAlign: "left",
       align: "left",
-      cellClassName: "name-column-cell",
     },
     {
       field: "phone",
       headerName: "Phone Number",
+      headerClassName: "headerColumns",
       flex: 1,
     },
     {
       field: "email",
       headerName: "Email",
+      headerClassName: "headerColumns",
       flex: 1,
     },
     {
-      field: "access",
+      field: "accessLevel",
       headerName: "Access Level",
+      headerClassName: "headerColumns",
       flex: 1,
       renderCell: ({ row: { access } }) => {
         return (
           <Box
             width="60%"
-            m="0 auto"
+            m="10px auto"
             p="5px"
-            diplay="flex"
+            display="flex"
             justifyContent="center"
             backgroundColor={
               access === "admin"
                 ? colors.greenAccent[600]
+                : access === "manager"
+                ? colors.greenAccent[700]
                 : colors.greenAccent[700]
             }
             borderRadius="4px"
@@ -58,7 +64,9 @@ const Team = () => {
             {access === "admin" && <AdminPanelSettingsOutlined />}
             {access === "manager" && <SecurityOutlined />}
             {access === "user" && <LockOutlined />}
-            <Typography color={colors.grey[100]}>{access}</Typography>
+            <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
+              {access}
+            </Typography>
           </Box>
         );
       },
@@ -66,10 +74,38 @@ const Team = () => {
   ];
 
   return (
-    <Box>
+    <Box m="20px">
       <Header title="TEAM" subtitle="Managing the Team Members" />
-      <Box>
-        <DataGrid rows={mockDataTeam} columns={columns} />
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+          "& .headerColumns": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid checkboxSelection rows={mockDataTeam} columns={columns} />
       </Box>
     </Box>
   );
